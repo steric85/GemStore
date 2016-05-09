5ede0eaf6416cdbdc73303946791f5eb6c69403c
steric85 <aneemeh@gmail.com>
2016-05-10 00:17:58 +0530

    Done with level 2

--- app.js
+++ app.js
@@ -10,12 +10,32 @@
 		price : 2.95,
 		description : 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodecahedron is one of those gems.',
 		canPurchase : true,
+		images : [{
+			full : "dodecahedron-01-full.jpg",
+			thumb : "dodecahedron-01-thumb.jpg",
+		}],
 	},
 	{
 		name : 'Pentagonal Gem',
 		price : 5.95,
-		description : 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodecahedron is one of those gems.',
+		description : 'Some gems have hidden qualities beyond their luster, beyond their shine... Pentagonal is one of those gems.',
 		canPurchase : false,	
+		images : [{
+			full : "pentagon-01-full.jpg",
+			thumb : "pentagon-01-thumb.jpg",
+		}],	
 	}];
+
+	app.controller('PanelController',function(){
+		this.tab = 1;
+
+		this.selectTab=function(checkTab){
+			this.tab=checkTab;
+		};
+
+		this.isSelected=function(checkTab){
+			return this.tab===checkTab;
+		}
+	});
 
 })();
